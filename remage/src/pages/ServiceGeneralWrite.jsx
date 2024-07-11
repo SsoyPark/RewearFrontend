adcf1b4a3514adda714e308e70d6533795983db6
revivalchg <revivalchg@gmail.com>
2024-07-11 15:28:23 +0900

    feat: 개인 사용자 주문서 작성 페이지 수정

--- remage/src/pages/ServiceGeneralWrite.jsx
+++ remage/src/pages/ServiceGeneralWrite.jsx
@@ -2,14 +2,49 @@ import React from "react";
 import { useState } from "react";
 // import Button from "../components/common/Button";
 import ImageUploader from "../components/common/ImageUploader";
+import Button from "../components/common/Button";
 import FormInput from "../components/common/FormInput";
 import Textarea from "../components/common/Textarea";
+import SelectField from "../components/common/SelectField";
 import { useNavigate } from "react-router-dom";
 import "./ServiceGeneralWrite.css";
 
 const ServiceGeneralWrite = () => {
   const navigate = useNavigate();
   const [selectedUserType, setSelectedUserType] = useState("general");
+
+  const sleeveOptions = [
+    { value: "short", label: "짧은소매" },
+    { value: "long", label: "긴소매" },
+    { value: "sleeveless", label: "민소매" },
+  ];
+
+  const necklineOptions = [
+    { value: "collar", label: "카라" },
+    { value: "round", label: "라운드넥" },
+    { value: "vneck", label: "브이넥" },
+    { value: "square", label: "스퀘어넥" },
+    { value: "turtle", label: "터틀넥" },
+    { value: "mock", label: "모크넥" },
+  ];
+
+  const pocketOptions = [
+    { value: "none", label: "없음" },
+    { value: "left", label: "왼쪽" },
+    { value: "right", label: "오른쪽" },
+  ];
+
+  const materialOptions = [
+    { value: "none", label: "없음" },
+    { value: "buttons", label: "단추" },
+    { value: "zippers", label: "지퍼" },
+  ];
+
+  const otherOptions = [
+    { value: "crop", label: "크롭" },
+    { value: "resize", label: "폼 줄이기" },
+    { value: "others", label: "그 외" },
+  ];
 
   return (
     <div className="page service-general-write">
@@ -25,89 +60,94 @@ const ServiceGeneralWrite = () => {
                         <span className="step round"><span>3.<span className="space"></span>주문 완료</span></span>
                     </div>
                     <div className="content">
+                        {/* 의류 사진 업로드 영역 시작 */}
                         <h3 className="paragraph-title">리폼할 의류 사진</h3>
-                            <ImageUploader />
+                        <ImageUploader />
+                        <Button text="이미지 분석하기" />
+                        {/* 의류 사진 업로드 영역 끝 */}
+                        {/* 의류 정보 출력 영역 시작 */}
                         <h3 className="paragraph-title">의류 정보</h3>
-                        <div className="clothes-info col-container">
-                            <div className="col">
-                                <FormInput
-                                    id="clothes-category"
-                                    label="카테고리"
-                                    value={' '}
-                                />
-                                <FormInput
-                                    id="clothes-sleeve"
-                                    label="소매길이"
-                                    value={' '}
-                                />
-
+                        <div className="clothes-info form-container">
+                            <div className="form-item">
+                                <div>
+                                    <label>카테고리</label>
+                                    <div><p>티셔츠</p></div>
+                                </div>
                             </div>
-                            <div className="col">
-                                <FormInput
-                                    id="clothes-neckline"
-                                    label="넥라인"
-                                    value={' '}
-                                />
-                                <FormInput
-                                    id="clothes-pocket"
-                                    label="주머니"
-                                    value={' '}
-                                />
-                                <FormInput
-                                    id="clothes-others"
-                                    label="부자재"
-                                    value={' '}
-                                />
-
+                            <div className="form-item">
+                                <div>
+                                    <label>넥라인</label>
+                                    <div><p>라운드넥</p></div>
+                                </div>
+                            </div>
+                            <div className="form-item">
+                                <div>
+                                    <label>소매길이</label>
+                                    <div><p>짧은소매</p></div>
+                                </div>
+                            </div>
+                            <div className="form-item">
+                                <div>
+                                    <label>주머니</label>
+                                    <div><p>없음</p></div>
+                                </div>
+                            </div>
+                            <div className="form-item">
+                                <div>
+                                    <label>부자재</label>
+                                    <div><p>없음</p></div>
+                                </div>
                             </div>
                         </div>
+                        {/* 의류 정보 출력 영역 끝 */}
+                        {/* 리폼 요청사항 입력 영역 시작 */}
                         <h3 className="paragraph-title">리폼 요청사항</h3>
-                        <div className="reform-info col-container">
-                            <div className="col">
-                                <FormInput
-                                    id="clothes-sleeve"
+                        <div className="reform-info form-container">
+                            <div className="form-item">
+                                <SelectField
                                     label="소매길이 수정"
-                                    value={' '}
-                                />
+                                    options={sleeveOptions}
+                                    placeholder="소매길이 선택"
+                                 />
 
                             </div>
-                            <div className="col">
-                                <FormInput
-                                    id="clothes-neckline"
+                            <div className="form-item">
+                                <SelectField
                                     label="넥라인 변경"
-                                    value={' '}
-                                />
+                                    options={necklineOptions}
+                                    placeholder="넥라인 선택"
+                                 />
                             </div>
-                            <div className="col">
-                                <FormInput
-                                    id="clothes-pocket"
-                                    label="주머니 수정"
-                                    value={' '}
-                                />
-                            </div>
-                            <div className="col">
-                                <FormInput
-                                    id="clothes-others"
+                            <div className="form-item">
+                                <SelectField
                                     label="부자재 수정"
-                                    value={' '}
-                                />
+                                    options={materialOptions}
+                                    placeholder="부자재 선택"
+                                 />
                             </div>
-                            <div className="new-line">
-                                <FormInput
-                                    id="clothes-materials"
-                                    label="기타 요청사항 선택"
-                                    value={' '}
-                                />
+                            <div className="form-item">
+                                <SelectField
+                                    label="주머니 선택"
+                                    options={pocketOptions}
+                                    placeholder="주머니 선택"
+                                 />
+                            </div>
+                            <div className="form-item">
+                                <SelectField
+                                    label="기타 (선택)"
+                                    options={otherOptions}
+                                    placeholder="기타 선택"
+                                 />
                             </div>
                             <div className="new-line full-width-input">
                                 <Textarea
                                     id="clothes-others"
-                                    placeholder=" "
+                                    placeholder="요청사항을 입력해주세요."
                                 />
-
-
                             </div>
                         </div>
+                        {/* 리폼 요청사항 입력 영역 끝 */}
+                        <Button text="다음" className="form-next" />
                     </div>
                 </div>
             </div>
