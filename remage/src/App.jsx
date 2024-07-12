819e0d9105fc76d537ef003039d35c42af98f3ef
mabagit config --global user.name maba <gyj0149@gmail.com>
2024-07-12 14:57:00 +0900

    refactor: 마이페이지 폴더 구조

--- remage/src/App.jsx
+++ remage/src/App.jsx
@@ -14,7 +14,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Mypage from "./pages/Mypage/Mypage";
-import Mypageorderlist from "./pages/MypageOrderlist";
 import Board from "./pages/Board";
 import EditProfile from "./pages/Mypage/EditProfile";
 
@@ -41,7 +40,7 @@ function App() {
             <Route path="/sign-up/complete" element={<Completion />} />
             <Route path="/mypage/*" element={<Mypage />} />
             <Route path="/mypage/edit" element={<EditProfile />} />
-            <Route path="/mypage/orderlist" element={<Mypageorderlist/>}/>
+            {/* <Route path="/mypage/orderlist" element={<Mypageorderlist/>}/> */}
             {/* <Route path="/login/general/" element={<LoginGeneral />} />
             <Route path="/login/company/" element={<LoginCompany />} /> */}
             <Route path="/service/" />
---
+++ remage/src/pages/MyPage/MypageOrderlist.jsx
@@ -0,0 +1,172 @@
+import React, { useState } from "react";
+import { FaChevronRight } from "react-icons/fa";
+import "./MypageOrderlist.css";
+import "./Mypage.css";
+
+const orders = [
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {
+    /*이미지 정보가 필요하다*/
+    date: "저장 날짜: 2024.0.00",
+    description: "청바지 리폼1 부탁드립니다.",
+    detail:
+      "가능하면 최대한 워싱 부분 살렸으면 좋겠어요. 단추도 디테일로 살릴 수 있으면 살리고 싶습니다.",
+    status: "임시저장",
+    /*주문상세 링크도 필요한가..? 어떻게 하는지 모르겠다*/
+    url: "/order/1",
+  },
+  {
+    date: "주문 날짜: 2024.0.00",
+    description: "청바지 리폼2 부탁드립니다.",
+    detail:
+      "가능하면 최대한 워싱 부분 살렸으면 좋겠어요. 단추도 디테일로 살릴 수 있으면 살리고 싶습니다.",
+    status: "주문대기",
+    url: "/order/2",
+  },
+  {
+    date: "저장 날짜: 2024.0.00",
+    description: "청바지 리폼3 부탁드립니다.",
+    detail:
+      "가능하면 최대한 워싱 부분 살렸으면 좋겠어요. 단추도 디테일로 살릴 수 있으면 살리고 싶습니다.",
+    status: "주문완료",
+    url: "/order/3",
+  },
+  {
+    date: "주문 날짜: 2024.0.00",
+    description: "청바지 리폼4 부탁드립니다.",
+    detail:
+      "가능하면 최대한 워싱 부분 살렸으면 좋겠어요. 단추도 디테일로 살릴 수 있으면 살리고 싶습니다.",
+    status: "주문대기",
+    url: "/order/4",
+  },
+  {
+    date: "저장 날짜: 2024.0.00",
+    description: "청바지 리폼5 부탁드립니다.",
+    detail:
+      "가능하면 최대한 워싱 부분 살렸으면 좋겠어요. 단추도 디테일로 살릴 수 있으면 살리고 싶습니다.",
+    status: "임시저장",
+    url: "/order/5",
+  },
+  {
+    date: "주문 날짜: 2024.0.00",
+    description: "청바지 리폼6 부탁드립니다.",
+    detail:
+      "가능하면 최대한 워싱 부분 살렸으면 좋겠어요. 단추도 디테일로 살릴 수 있으면 살리고 싶습니다.",
+    status: "배송완료",
+    url: "/order/6",
+  },
+  {
+    date: "저장 날짜: 2024.0.00",
+    description: "청바지 리폼7 부탁드립니다.",
+    detail:
+      "가능하면 최대한 워싱 부분 살렸으면 좋겠어요. 단추도 디테일로 살릴 수 있으면 살리고 싶습니다.",
+    status: "임시저장",
+    url: "/order/7",
+  },
+  {
+    image: "1.jpg" /* 백에서 api를 통해 가져와줄거라 믿어봅니다*/,
+    date: "주문 날짜: 2024.0.00",
+    description: "청바지 리폼8 부탁드립니다.",
+    detail:
+      "가능하면 최대한 워싱 부분 살렸으면 좋겠어요. 단추도 디테일로 살릴 수 있으면 살리고 싶습니다.",
+    status: "주문완료",
+  },
+  /*추가 주문 내역이 있으면 여기 생성될 수 있도록 구성*/
+];
+
+const MypageOrderlist = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+
+  // 현재 페이지에 해당하는 아이템을 계산
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentOrders = orders
+    .slice()
+    .reverse()
+    .slice(indexOfFirstItem, indexOfLastItem);
+
+  // 페이지 변경 핸들러
+  const handleClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // 페이지 번호 계산
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(orders.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    // <div className="mypage-orderlist">
+    <main className="main-content">
+      <h2 className="h2 title">주문내역</h2>
+      <p className="order-sub-title">
+        내 주문 <span className="myorder-count">{orders.length}건</span>
+      </p>
+      <div className="order_list">
+        {currentOrders.map((order, index) => (
+          <div key={index} className="order_item">
+            <img
+              src={order.image || "default-image.jpg"}
+              className="order_image"
+            />
+            <div className="order_box">
+              <p className="order_date">{order.date}</p>
+              <p className="order_title">{order.description}</p>
+              <p className="order_detail">{order.detail}</p>
+            </div>
+            <div className="order_status">{order.status}</div>
+            {/* 주문 연결이 필요해요. */}
+            <div className="view_order_details">
+              주문상세보기 <FaChevronRight />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="pagination">
+        {currentPage !== 1 && (
+          <button
+            onClick={() => handleClick(currentPage - 1)}
+            className="page-number"
+          >
+            &lt;
+          </button>
+        )}
+        {pageNumbers.map((number) => (
+          <button
+            key={number}
+            onClick={() => handleClick(number)}
+            className={`page-number ${number === currentPage ? "active" : ""}`}
+          >
+            {number}
+          </button>
+        ))}
+        {currentPage !== pageNumbers.length && (
+          <button
+            onClick={() => handleClick(currentPage + 1)}
+            className="page-number"
+          >
+            &gt;
+          </button>
+        )}
+      </div>
+    </main>
+    // </div>
+  );
+};
+
+export default MypageOrderlist;
