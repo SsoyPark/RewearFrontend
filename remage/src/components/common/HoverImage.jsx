11e71b187ab033beca026df644d6742e95fa51d4
revivalchg <revivalchg@gmail.com>
2024-07-17 23:11:16 +0900

    fix: HoverImage 컴포넌트 위에 마우스오버 시 이미지가 정상적으로 나타나지 않는 문제 해결, 스타일 수정

--- remage/src/components/common/HoverImage.jsx
+++ remage/src/components/common/HoverImage.jsx
@@ -1,34 +1,40 @@
 import React, { useState } from 'react';
-import "./HoverImage.css";
+import classNames from 'classnames';
+import styles from './HoverImage.module.css';
 
-function HoverImage({imgUrl, imgAlt, hoverText}) {
-    const [isVisible, setIsVisible] = useState(false);  // 이미지 표시 상태
+const HoverImage = ({
+  imgUrl,
+  imgAlt,
+  hoverText,
+  className
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+  console.log('Is Visible', isVisible);
+  const classArray = className ? className.split(" ") : [];
 
-    return (
-        <span>
-            {/* 텍스트에 마우스 오버/아웃 이벤트 핸들러 추가 */}
-            <span 
-                onMouseOver={() => setIsVisible(true)} 
-                onMouseOut={() => setIsVisible(false)}
-                style={{ cursor: 'pointer' }}
-            >
-                {hoverText}
-            </span>
+  const componentClass = classNames(
+    styles["component"],
+    ...classArray.map(cls => styles[cls])
+  );
 
-            {/* 이미지 표시 상태에 따라 이미지 컴포넌트를 조건부 렌더링 */}
-            {isVisible && (
-                <img 
-                    src={imgUrl} 
-                    alt={imgAlt}
-                    // style={{
-                    //     width: '100px', // 이미지 크기 설정
-                    //     position: 'absolute', // 이미지 위치 조정
-                    //     left: '100%', // 텍스트 오른쪽에 이미지 표시
-                    // }}
-                />
-            )}
-        </span>
-    );
-}
+  return (
+    <div className={componentClass}>
+      <span 
+        onMouseOver={() => setIsVisible(true)} 
+        onMouseOut={() => setIsVisible(false)}
+      >
+        {hoverText}
+      </span>
+
+      {isVisible && (
+        <img 
+          src={imgUrl}
+          alt={imgAlt}
+          className={styles.imageStyle}
+        />
+      )}
+    </div>
+  );
+};
 
 export default HoverImage;
