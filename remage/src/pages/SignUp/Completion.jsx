bed7c9e30724bd40798cb5188ed372bf570da755
Maba <gigig9647@gmail.com>
2024-07-19 02:36:34 -0300

    feature: 회원가입 완료 작성중

--- remage/src/pages/SignUp/Completion.jsx
+++ remage/src/pages/SignUp/Completion.jsx
@@ -3,11 +3,24 @@ import { FiCheckCircle } from "react-icons/fi";
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./Completion.module.css";
 import Button from "../../components/common/Button";
+import useAuthStore from "../../stores/useAuthStore";
+import { loginService } from "../../service/user";
 
 const Completion = () => {
+  const { login } = useAuthStore();
   const [isValidAccess, setIsValidAccess] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+  const userId = location.state?.userId
+  const password = location.state?.password
+  const userType = location.state?.userType
+  const handleLoginClick = async () => {
+    try {
+      await loginService(userId, password, navigate, login, userType)
+    } catch (err) {
+      alert(err.message);
+    }
+  };
   useEffect(() => {
     if (location.state?.from !== "UserInfo") {
       alert("일반적이지 않은 접근입니다!");
@@ -28,8 +41,9 @@ const Completion = () => {
             </p>
             <Button
               url="/"
-              text="홈으로 돌아가기"
+              text="로그인 하기"
               className="sign-up-complete"
+              onClick={handleLoginClick}
             ></Button>
           </div>
         </div>
