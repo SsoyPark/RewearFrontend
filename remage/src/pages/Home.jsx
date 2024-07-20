9bd5cc87dabbd187832f5df8b7bea40abea49e98
ssoypark <c912t117@gmail.com>
2024-07-20 14:22:00 +0900

    fix:mypage

--- remage/src/pages/Home.jsx
+++ remage/src/pages/Home.jsx
@@ -1,37 +1,39 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import useAuthStore from "../stores/useAuthStore";
 import Button from "../components/common/Button";
 import Banner from '../components/Banner';
 
 const Home = () => {
-  const { login, logout } = useAuthStore();
-  const { isAuthenticated } = useAuthStore();
-  const isAuthenticated2 = useAuthStore((state) => state.isAuthenticated);
+  const navigate = useNavigate();
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
-  // return (
-  //   <div className="home-container">
-  //     <h1>Home</h1>
-  //     <h1>{isAuthenticated2 ? "Authenticated" : "Not Authenticated"}</h1>
-  //     <h1>{isAuthenticated ? "Authenticated" : "Not Authenticated"}</h1>
-  //     <Button text={isAuthenticated?"임시 로그아웃":"임시로그인"} onClick={isAuthenticated?logout:()=>login("sample", "sample")}/>
-      
-  //   </div>
-  // );
+  const handleButtonClick = () => {
+    if (isAuthenticated) {
+      navigate("/service/general/write/");
+    } else {
+      navigate("/login/*");
+    }
+  };
 
-  return (
+
+
+  return (      
     <div className="home-page">
       <div className="main-text">
         <p className="red">UPCYCLE</p>
         <p className="black">YOUR</p>
         <p className="black">CLOTHING</p>
-        <Button className="try-button" text="Try Our Service"></Button>
+        <Button 
+          className="try-button" 
+          text="Try Our Service" 
+          onClick={handleButtonClick} // 버튼 클릭 핸들러 추가
+        />
       </div>
       <Banner />
     </div>
   );
-
-
 };
 
 export default Home;
