fd25239c18008a9a531701611574830d1610610b
ssoypark <c912t117@gmail.com>
2024-07-21 10:08:36 +0900

    fix:banner

--- remage/src/components/Banner.jsx
+++ remage/src/components/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
               <img src="/banner1-2.png" alt="Banner 1-2" className="banner-image" />
             </div>
             <div className="banner-text">
-              <p className="banner-text title">Upcycle Clothes</p>
+              <h3>Upcycle Clothes</h3>
               <p className="banner-text content">생성형AI로 원하는 리폼 디자인을 추천받으세요</p>
             </div>
           </div>
@@ -20,7 +20,7 @@ const Banner = () => {
         <div className="banner-item right">
           <div className="banner-content">
             <div className="banner-text">
-              <p className="banner-text title">Virtual Fitting</p>
+              <h3>Virtual Fitting</h3>
               <p className="banner-text content">선택한 리폼 디자인으로 가상피팅 해보세요</p>
             </div>
             <img src="/human2.png" alt="Banner 2" className="banner-image resize" />
